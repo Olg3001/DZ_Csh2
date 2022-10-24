6f41fda5c500f7f8b6e5448f7535bd5c4c60ce7d
Olga <olg3001@yandex.ru>
2022-10-24 16:47:34 +0300

    DZ11-2

---
+++ DZ11-Sec/Program.cs
@@ -0,0 +1,34 @@
+﻿// 1 программист, 2 программиста, 5 программистов
+// TwoLastDigits для чисел 11-14
+
+Console.WriteLine("Введите число программистов в комнате: ");
+int NumberOfProgrammists = int.Parse(Console.ReadLine()!);
+
+int LastDigit = NumberOfProgrammists % 10;
+int TwoLastDigits = NumberOfProgrammists % 100;
+
+void ChooseLastDigits(int NumberOfProgrammists, int LastDigit)
+{
+    if (TwoLastDigits > 10 && TwoLastDigits < 15)
+    {
+        Console.WriteLine(NumberOfProgrammists + " програмистов");
+    }
+
+    else
+    {
+        if (LastDigit == 1)
+        {
+            Console.WriteLine(NumberOfProgrammists + " программист");
+        }
+        if (LastDigit > 1 && LastDigit < 5)
+        {
+            Console.WriteLine(NumberOfProgrammists + " программиста");
+        }
+        if (LastDigit > 4 || LastDigit == 0)
+        {
+            Console.WriteLine(NumberOfProgrammists + " программистов");
+        }
+    }
+}
+
+ChooseLastDigits(NumberOfProgrammists, LastDigit);
