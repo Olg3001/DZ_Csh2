c9c0ec09b20a853b6a2ffb1606369c9783a286d6
Olga <olg3001@yandex.ru>
2022-10-24 16:55:12 +0300

    DZ13

---
+++ DZ13/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+// что третьей цифры нет.Через строку решать нельзя.
+// 645 -> 5   78 -> третьей цифры нет
+
+Console.WriteLine("Введите трехзначное или большее число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+if (number < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+
+else
+{
+    FindThirdDigit(number);
+}
+
+void FindThirdDigit(int number)
+{
+    while (number > 999)
+    {
+        number = number / 10;
+    }
+
+    int result(int number)
+    {
+        int thirdDigit = number % 10; 
+        return thirdDigit;
+    }
+    Console.WriteLine("Третья цифра введенного числа: " + result(number));
+}
